fe266f3c4ff15d3e6ea4c3aa48a932d24833cc5f
Shane Freed <shane.freed@gmail.com>
2024-10-29 14:56:11 -0400

    added announcements

--- src/app/features/announcements/announcements.component.ts
+++ src/app/features/announcements/announcements.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
+import { AnnouncementModel } from 'impactdisciplescommon/src/models/domain/announcement.model.ts';
+import { EventModel } from 'impactdisciplescommon/src/models/domain/event.model';
+import { EventAnnouncementService } from 'impactdisciplescommon/src/services/data/event-announcement.service';
+import { map, Observable } from 'rxjs';
+import { DataService } from 'src/app/admin/data.service';
 
 @Component({
   selector: 'app-announcements',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./announcements.component.css']
 })
 export class AnnouncementsComponent implements OnInit {
+  @Input('event') event: EventModel;
 
-  constructor() { }
+  announcements: AnnouncementModel[];
 
-  ngOnInit() {
+  constructor(private service: EventAnnouncementService,
+    private dataService: DataService
+  ) { }
+
+  async ngOnInit() {
+    this.event = await this.dataService.getEvent();
+
+    if(this.event?.id){
+      this.service.streamAllByValue('eventId', this.event.id).subscribe(announcements => this.announcements = announcements)
+    }
   }
 
 }
