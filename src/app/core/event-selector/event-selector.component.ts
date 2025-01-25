271935873ccd317e9e722e0c8ed884e790118296
Shane Freed <shane.freed@gmail.com>
2025-01-25 11:22:46 -0500

    added checks for multiple emails

--- src/app/core/event-selector/event-selector.component.ts
+++ src/app/core/event-selector/event-selector.component.ts
@@ -2,7 +2,7 @@ import { AuthService } from 'impactdisciplescommon/src/services/utils/auth.servi
 import { DataService } from './../../admin/data.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { WhereFilterOperandKeys } from 'impactdisciplescommon/src/dao/firebase.dao';
+import { QueryParam, WhereFilterOperandKeys } from 'impactdisciplescommon/src/dao/firebase.dao';
 import { EventRegistrationModel } from 'impactdisciplescommon/src/models/domain/event-registration.model';
 import { EventModel } from 'impactdisciplescommon/src/models/domain/event.model';
 import { EventService } from 'impactdisciplescommon/src/services/data/event.service';
@@ -22,6 +22,10 @@ export class EventSelectorComponent implements OnInit {
 
   registrations: EventRegistrationModel[]
 
+  emailAddressUsed: string;
+
+  errorFound:boolean = false;
+
   constructor(private cookieService: CookieService,
     private eventService: EventService,
     private eventRegistrationService: EventRegistrationService,
@@ -40,17 +44,18 @@ export class EventSelectorComponent implements OnInit {
     let registeredEvents: EventModel[] = await this.eventService.queryAllByValue('id', WhereFilterOperandKeys.in, ids);
 
     if(registeredEvents && registeredEvents.length == 1){
+      this.errorFound = false;
       this.dataService.initializeEvent(registeredEvents[0]);
-      this.setUserCookie(registeredEvents[0]).pipe(take(1)).subscribe(reg => {this.router.navigate(['home'])});
+      this.checkForMultipleRegistrations(registeredEvents[0]);
     } else {
       this.registeredEventsList = registeredEvents;
     }
   }
 
   selectEvent(event: EventModel){
+    this.errorFound = false;
     this.dataService.initializeEvent(event);
-    this.setUserCookie(event).pipe(take(1)).subscribe(reg => {this.router.navigate(['home'])});
-
+    this.checkForMultipleRegistrations(event);
   }
 
   setUserCookie(event:EventModel){
@@ -62,6 +67,24 @@ export class EventSelectorComponent implements OnInit {
     this.eventRegistrationService.update(registration.id, registration);
 
     return this.authService.setUser(registration);
+  }
+
+  checkForMultipleRegistrations(event:EventModel){
+    this.setUserCookie(event).pipe(take(1)).subscribe(reg => {
+      this.emailAddressUsed = reg.email.toLowerCase();
+
+      let params: QueryParam[] = [];
+      params.push(new QueryParam('email', WhereFilterOperandKeys.equal, this.emailAddressUsed));
+      params.push(new QueryParam('eventId', WhereFilterOperandKeys.equal, event.id));
+
+      this.eventRegistrationService.queryAllByMultiValue(params).then(registrations => {
+        if(registrations && registrations.length == 1){
+          this.router.navigate(['home'])
+        } else {
+          this.errorFound = true;
+        }
+      })
+    });
   }
 
 }
